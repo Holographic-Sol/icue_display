a5cc4a9578f5fd7164115562a025d77b151fef1d
Sol <36607894+Holographic-Sol@users.noreply.github.com>
2021-04-05 15:29:44 +0100

    Add files via upload

--- iCUEDisplay.py
+++ iCUEDisplay.py
@@ -2151,12 +2151,8 @@ class NetworkMonClass(QThread):
         global network_adapter_display_snt_bool
         network_adapter_exists_bool = False
         try:
-            network_adapter_display_rcv_bool = []
-            network_adapter_display_snt_bool = []
-            for _ in net_rcv_led:
-                network_adapter_display_rcv_bool.append(False)
-            for _ in net_snt_led:
-                network_adapter_display_snt_bool.append(False)
+            network_adapter_display_rcv_bool = [False, False, False, False, False, False, False, False, False]
+            network_adapter_display_snt_bool = [False, False, False, False, False, False, False, False, False]
             rec_item = ''
             sen_item = ''
             obj_wmi_service = win32com.client.Dispatch("WbemScripting.SWbemLocator")
